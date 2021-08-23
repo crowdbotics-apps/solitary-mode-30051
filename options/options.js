392aa2c73f4d1b3237d9bcfbb24f856a85fe7d48
Crowdbotics <team@crowdbotics.com>
2021-08-23 17:42:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "solitary_mode_30051Identifier",
+  url: "https://solitary_mode_30051Identifier.botics.co",
+  api: "https://solitary_mode_30051Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
